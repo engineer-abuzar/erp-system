97e4b3f5d4665cf8cf01e1783148525fc0b87622
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2025-10-29 15:46:58 +0530

    Bakcend changes

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 const authentication=(req,res,next)=>{
     const token=req.headers.authorization.split(' ')[1]
     try{
+        console.log("Token is : ",token)
     const result=jwt.verify(token,process.env.JWT_SECRET_KEY)
     next()
     }catch{
