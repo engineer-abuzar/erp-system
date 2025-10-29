9c38bfd28a7d3eede5505d1e486bdc911669851e
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2025-10-29 14:45:04 +0530

    Changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
                   <Route path="Dues" element={<Dues />} />
                 </Route>
               </>
-              : <Route path='/login' element={<Login />} />}
+              : <Route path='/login' element={<Dues />} />}
 
 
 
