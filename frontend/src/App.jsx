a285fe487d30507af8c9a3ed1f9601032aafa4c2
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2025-10-29 15:39:36 +0530

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,8 @@ const App = () => {
             <Route path='/' element={<Landing />} />
             <Route path='*' element={<NotFound />} />
 
-          
+            {auth ?
+              <>
                 <Route path='/login' element={<AuthLayoutAdmin />} />
                 <Route path='/loggedin' element={<AuthLayoutAdmin />} >
                   <Route path="Students" element={<Students />}>
@@ -57,8 +58,8 @@ const App = () => {
                   <Route path="Teachers" element={<Teachers />} />
                   <Route path="Dues" element={<Dues />} />
                 </Route>
-       
-              {/* : <Route path='/login' element={<Login />} />} */}
+              </>
+              : <Route path='/login' element={<Login />} />}
 
 
 
